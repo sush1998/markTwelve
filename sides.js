1eda6aafa99344c18d7afaef62e0c7410cbe944f
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-07-29 21:45:41 +0530

    feat: click back navigates to home.html

--- sides.js
+++ sides.js
@@ -1,7 +1,7 @@
 const side_a=document.querySelector("#side_a")
 const side_b=document.querySelector("#side_b")
 const btn_calculate=document.querySelector("#calculate")
-
+const backToHome=document.querySelector("#back_to_home")
 const out=document.querySelector("#out")
 
 
@@ -22,4 +22,9 @@ btn_calculate.addEventListener('click',(event)=>
     let a=parseInt(side_a.value);
     let b=parseInt(side_b.value)
     calculateHypo(a,b)
+})
+
+backToHome.addEventListener("click",()=>
+{
+    window.location.href="index.html"
 })
