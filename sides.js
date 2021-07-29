60b4b5f0d9e2e845b6037699ee5e8ed5e920f2d4
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-07-29 16:39:17 +0530

    feat: calculate hypotenuse and display

---
+++ sides.js
@@ -0,0 +1,25 @@
+const side_a=document.querySelector("#side_a")
+const side_b=document.querySelector("#side_b")
+const btn_calculate=document.querySelector("#calculate")
+
+const out=document.querySelector("#out")
+
+
+function calculateHypo(a,b)
+{
+    console.log("calculating")
+    console.log(a+' '+b)
+    let hypo=Math.sqrt((a*a)+(b*b));
+    console.log("hypo :"+hypo)
+    out.textContent=hypo
+}
+
+
+btn_calculate.addEventListener('click',(event)=>
+{
+    event.preventDefault();
+    console.log("clicked");
+    let a=parseInt(side_a.value);
+    let b=parseInt(side_b.value)
+    calculateHypo(a,b)
+})
