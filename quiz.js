5e730ebea88f4a731f406e6e5ce44f1ae6344544
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-07-31 20:04:22 +0530

    feat: check form inputs with answers

--- quiz.js
+++ quiz.js
@@ -1,1 +1,21 @@
-const answers=[]
+const answers=["isosceles","one","35mm","55","false"]
+
+const quizForm=document.querySelector("#quizForm")
+const questions=document.querySelectorAll(".question_container")
+const btn_quiz_submit=document.querySelector("#btn_quiz_submit")
+
+const selected_options=document.q
+
+
+btn_quiz_submit.addEventListener('click',(event)=>
+{
+    event.preventDefault()
+    console.group("clicked")
+    let pos=0
+    let data=new FormData(quizForm)
+    for(let entry of data)
+    {
+        console.log(entry[1]==answers[pos])
+        pos+=1
+    }
+})
