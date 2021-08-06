c7e55b6ae15db5c70f34023e42350e2f69320bb9
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-08-06 18:22:09 +0530

    feat: sides validation done

--- sides.js
+++ sides.js
@@ -4,6 +4,7 @@ const btn_calculate=document.querySelector("#calculate")
 const backToHome=document.querySelector("#back_to_home")
 const out=document.querySelector("#out")
 const output_div=document.querySelector(".output")
+const err_msg_side=document.querySelector("#err_msg_side")
 
 function calculateHypo(a,b)
 {
@@ -20,6 +21,14 @@ btn_calculate.addEventListener('click',(event)=>
 {
     event.preventDefault();
     console.log("clicked");
+    if((side_a.valuea===""||side_a.value<=0)||(side_b.valuea===""||side_b.value<=0))
+    {
+        err_msg_side.style.display="block"
+        output_div.style.display="none"
+        err_msg_side.textContent="Please enter valid inputs"
+        return;
+    }
+    err_msg_side.style.display="none"
     let a=parseInt(side_a.value);
     let b=parseInt(side_b.value)
     calculateHypo(a,b)
